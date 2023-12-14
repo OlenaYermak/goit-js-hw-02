174c5029fc7e801f7638dfae9de2786edee941df
OlenaYermak <145419464+OlenaYermak@users.noreply.github.com>
2023-12-14 14:17:33 +0200

    add

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,22 @@
 'use strict';
-function getElementWidth(content, padding, border) {
-    content = Number.parseFloat(content);
-    padding = Number.parseFloat(padding);
-    border = Number.parseFloat(border);
-    return content + padding * 2 + border * 2;
+function checkForSpam(message) {
+    const inputMessage = message.toLowerCase();
+ if (!inputMessage.includes("spam") && !inputMessage.includes("sale"))
+    {
+       return false;
+  } else {
+    return true;
+  }
 }
-console.log(getElementWidth("50px", "8px", "4px")); 
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px")); 
+
+ 
+
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
